cb88a9f9241454cd8ee0d42839d4da567ddaa773
Ella Yeonsu Choi <happymondaysu@gmail.com>
2023-05-15 14:06:54 +0900

    feat: Edit initial settings for starting project

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            개인정보 처리방침 | 이용 약관
+            All rights reserved @ Codestates
+        </footer>
+    );
+};
+
+export default Footer;
