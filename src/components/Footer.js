bf51eddd64c205adeff99acf657d98d70feec05b
Ella Yeonsu Choi <happymondaysu@gmail.com>
2023-05-15 15:00:12 +0900

    feat: Add footer feature

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,11 @@
+import '../components/Footer.css'
+
 import React from 'react';
 
 const Footer = () => {
     return (
-        <footer>
-            개인정보 처리방침 | 이용 약관
+        <footer className="footer">
+            개인정보 처리방침 | 이용 약관<br/>
             All rights reserved @ Codestates
         </footer>
     );
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import './index.css';
+import './components/index.css';
 
 import App from './App';
 import React from 'react';
